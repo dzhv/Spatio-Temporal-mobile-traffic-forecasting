5cdcce27aef92979652b3db6677190640833d184
dzhv <dz.vysniauskas@gmail.com>
2019-03-24 11:18:07 +0000

    Adjustments to mapping scripts and exploration notebook

--- data_grid_mapping.py
+++ data_grid_mapping.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 import milano_grid
 
+from datetime import datetime
+
 def map_to_tensor(data, grid_size):
 	# returns a 3D tensor of size (T, grid_height, grid_length)
 	# where T is the time axis
@@ -13,6 +15,7 @@ def map_to_tensor(data, grid_size):
 	i = 0
 	n_groups = len(groups)
 	for time, group in groups:
+		print(timestamp_string(time))
 		grid = np.zeros(grid_size)
 
 		for index, item in group.iterrows():
@@ -23,30 +26,51 @@ def map_to_tensor(data, grid_size):
 		grid_3d = np.expand_dims(grid, axis=0)
 		tensor = np.append(tensor, grid_3d, axis=0)
 
-
 		i += 1
 		print("{0}/{1} groups processed".format(i, n_groups))
 	
 	return tensor
 
-def map(data_path):
+def get_sorted_data(data_path):
+	print("reading the data from file")
 	data = pd.read_csv(data_path, delimiter="\t", header=None, 
 		usecols=[0, 1, 7], names=["square_id", "time", "internet"])
 
 	cleaned_data = data.dropna()
-	sorted_data = cleaned_data.sort_values(by="time")
+	return cleaned_data.sort_values(by="time")
 
+def map(data_path):
+	sorted_data = get_sorted_data(data_path)
 	tensor = map_to_tensor(sorted_data, milano_grid.size)
+
 	print(tensor.shape)
 	return tensor
 
-data_path = "data/sms-call-internet-mi-2013-11-01.txt" # "data/mini_data.dat" 
-save_path = "data/11-01_mapped.npy"
-
-def map_and_save():
+def map_and_save(data_path, save_path):
 	print("Mapping the data")
 	tensor = map(data_path=data_path)
 
 	print("Saving the data")
 	np.save(save_path, tensor)
+
+def timestamp_string(timestamp):
+	ts_in_seconds = int(timestamp) // 1000
+	return datetime.utcfromtimestamp(ts_in_seconds).strftime('%Y-%m-%d %H:%M:%S')
+
+def print_times(data_path):
+	sorted_data = get_sorted_data(data_path)
+
+	previous = None
+	indx = 0
+	for timestamp in sorted_data["time"]:
+		if timestamp != previous:			
+			previous = timestamp
+				
+			print("{0}, indx: {1}".format(timestamp_string(timestamp), indx))
+			indx += 1
+
+data_path = "data/sms-call-internet-mi-2013-11-01.txt" # "data/mini_data.dat" 
+save_path = "data/11-01_mapped.npy"
+
+# map(data_path)
 
--- tests/grid_map_test.py
+++ tests/grid_map_test.py
@@ -1,23 +1,39 @@
 import sys
 from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import numpy as np
 
 import data_grid_mapping
 
+def assertions(result):
+	assert result[0, 99, 0] == 2
+	assert result[1, 99, 0] == 3
+	assert result[2, 99, 0] == 4
+
+	assert result[0, 99, 0] == 2
+	assert result[1, 99, 0] == 3
+	assert result[2, 99, 0] == 4
+
+	assert result[0, 1, 98] == 2
+	assert result[1, 1, 98] == 0
+	assert result[2, 1, 98] == 4
 
 test_file = "raw_input.txt"
-result = data_grid_mapping.map(data_path=test_file)
-assert result[0, 99, 0] == 2
-assert result[1, 99, 0] == 3
-assert result[2, 99, 0] == 4
 
-assert result[0, 99, 0] == 2
-assert result[1, 99, 0] == 3
-assert result[2, 99, 0] == 4
+def test_map():	
+	result = data_grid_mapping.map(data_path=test_file)
+	assertions(result)
 
-assert result[0, 1, 98] == 2
-assert result[1, 1, 98] == 0
-assert result[2, 1, 98] == 4
+	print("test_map successful")
 
-print("test successful")
+def test_map_and_save():
+	save_path = "test.npy"
+	data_grid_mapping.map_and_save(test_file, save_path)
 
+	result = np.load(save_path)
+	assertions(result)
+
+	print("test_map_and_save successful")
+
+test_map()
+test_map_and_save()

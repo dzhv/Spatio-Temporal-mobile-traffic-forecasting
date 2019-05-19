d74fc5fea846198dd41f5d5da51ac0a6c82b6fe3
dzhv <dz.vysniauskas@gmail.com>
2019-05-19 10:21:39 +0100

    attempt at using multiple gpus

--- models/lstm.py
+++ models/lstm.py
@@ -2,9 +2,11 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, CuDNNLSTM
 from tensorflow.keras.layers import LSTM as CpuLSTM
 from tensorflow.keras.losses import mean_squared_error
+from tensorflow.keras.utils import multi_gpu_model
 from tensorflow.test import is_gpu_available
 from models.losses import nrmse_keras as nrmse
 from models.model import Model
+
 
 class LSTM(Model):
 
@@ -13,10 +15,12 @@ class LSTM(Model):
 		if is_gpu_available():
 			print("\nUsing CuDNNLSTM!\n")
 			self.model.add(CuDNNLSTM(hidden_size))
+			self.model.add(Dense(1))
+			self.model = multi_gpu_model(self.model)
 		else:
 			print("\nUsing CPU LSTM!\n")
 			self.model.add(CpuLSTM(hidden_size))
-		self.model.add(Dense(1))
+
 		self.model.compile(loss=mean_squared_error, optimizer='adam')
 
 		self.batch_size = batch_size

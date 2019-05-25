59fb8db155c627fca2d8715c16e81b692e985ea6
dzhv <dz.vysniauskas@gmail.com>
2019-05-25 10:34:27 +0100

    script for running a batch of lstm experiments

--- run_lstm_batch.py
+++ run_lstm_batch.py
@@ -6,4 +6,22 @@ num_layers=$2
 lr=$3
 hidden_size=$4
 """
-os.system("sbatch run_lstm.sh test 5 0.003 69")
+
+layers = [1, 2, 3]
+learning_rates = [0.001, 0.0001, 0.00005]
+hidden_sizes = [50]#, 75, 100]
+
+
+for n_layers in layers:
+	for lr in learning_rates:
+		for hidden_size in hidden_sizes:
+			experiment_name = "lstm_{0}layer_{1}lr_{2}hs".format(n_layers, str(lr).replace('.', ''), hidden_size)
+			print("experiment name: {0}".format(experiment_name))
+
+			command = "sbatch run_lstm.sh {0} {1} {2} {3}".format(
+				experiment_name, n_layers, lr, hidden_size)
+			print("command: {0}".format(command))
+
+			os.system(command)
+
+

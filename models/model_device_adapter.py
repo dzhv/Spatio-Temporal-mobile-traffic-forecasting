a3e1f902cf1efa52877850013332f45bc0207b95
dzhv <dz.vysniauskas@gmail.com>
2019-05-31 09:45:47 +0100

    trying to debug why i don't get multiple gpu model

--- models/model_device_adapter.py
+++ models/model_device_adapter.py
@@ -4,11 +4,18 @@ from tensorflow.test import is_gpu_available
 def get_device_specific_model(model, gpus):
 	""" utility function for preparing Keras models to use specific available devices
 	"""
+	print(f"is gpu available: {is_gpu_available()}")
+	print(f"gpus: {gpus}")
 	if is_gpu_available():
 		try:
+			print(f"trying to use {gpus} gpus")
 			model = multi_gpu_model(model, gpus)
 			print("\nUsing multiple gpus\n")
-		except:
+		except Exception as inst:
+			print(type(inst))
+			print(inst.args)
+			print(inst)
+
 			print("\nUsing single GPU\n")
 	else:
 		print("\nUsing CPU!\n")

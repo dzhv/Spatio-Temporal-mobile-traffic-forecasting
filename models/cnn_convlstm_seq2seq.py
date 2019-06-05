89e627dce05eb2c7e622e9e4d1f01d0bc0a28526
dzhv <dz.vysniauskas@gmail.com>
2019-06-05 10:42:37 +0100

    another experiment

--- models/cnn_convlstm_seq2seq.py
+++ models/cnn_convlstm_seq2seq.py
@@ -29,6 +29,7 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 
 		# encoder
 		out = ConvLSTM2D(filters=64, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
+		out = ConvLSTM2D(filters=64, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
 		encoder_outputs, state_h, state_c = ConvLSTM2D(filters=64, kernel_size=3, activation='tanh', 
 			padding='same', return_state=True)(out)
 
@@ -39,6 +40,7 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 		decoder_inputs = Input(shape=(segment_size,) + self.decoder_input_shape)
 		out = ConvLSTM2D(filters=64, kernel_size=3, return_sequences=True, activation='tanh', 
 			padding='same')([decoder_inputs, state_h, state_c])
+		out = ConvLSTM2D(filters=64, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
 		out = ConvLSTM2D(filters=32, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
 
 		out = TimeDistributed(Flatten())(out)
@@ -69,7 +71,7 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 	def form_targets(self, y):
 		return y[:, :, None]
 
-# model = CnnConvLSTMSeq2Seq(window_size=11)
-# output = model.forward(np.random.randn(1, 12, 11, 11))
-# print("output shape:")
-# print(output.shape)
+model = CnnConvLSTMSeq2Seq(window_size=11)
+output = model.forward(np.random.randn(1, 12, 11, 11))
+print("output shape:")
+print(output.shape)

41001bb083a1774341e99206d1453e2511005f67
dzhv <dz.vysniauskas@gmail.com>
2019-06-06 12:01:12 +0100

    cnn convlstm with dilated conv

--- models/cnn_convlstm_seq2seq.py
+++ models/cnn_convlstm_seq2seq.py
@@ -21,28 +21,30 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 		# 1 refers to a single channel of the input
 		encoder_inputs = Input(shape=(segment_size, window_size, window_size, 1))
 		
-		out = TimeDistributed(Conv2D(32, kernel_size=3, activation='tanh', padding='same'))(encoder_inputs)
-		out = TimeDistributed(AveragePooling2D())(out)
-		out = TimeDistributed(Conv2D(64, kernel_size=3, activation='tanh', padding='same'))(out)
-		out = TimeDistributed(AveragePooling2D())(out)
-		out = TimeDistributed(Conv2D(64, kernel_size=3, activation='tanh', padding='same'))(out)
+		out = TimeDistributed(Conv2D(25, kernel_size=3, activation='tanh', padding='same'))(encoder_inputs)
+		out = TimeDistributed(Conv2D(50, kernel_size=3, activation='tanh', padding='same'))(encoder_inputs)
+		out = TimeDistributed(Conv2D(50, kernel_size=3, dilation_rate=2, activation='tanh', padding='same'))(out)
+		out = TimeDistributed(Conv2D(50, kernel_size=3, dilation_rate=4, activation='tanh', padding='same'))(out)
 
 		# encoder
-		encoder_outputs, state_h, state_c = ConvLSTM2D(filters=64, kernel_size=3, activation='tanh', 
-			padding='same', return_state=True)(out)
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh')(out)
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh')(out)
+		encoder_outputs, state_h, state_c = ConvLSTM2D(filters=50, kernel_size=3, activation='tanh', 
+			return_state=True)(out)
 
 		# decoder
 		
-		latent_dim = window_size // 2 // 2  # accounting for average pooling operations
+		latent_dim = window_size - 2 * 3  # accounting for encoder conv operations
 		self.decoder_input_shape = (latent_dim, latent_dim, 50)
 		decoder_inputs = Input(shape=(segment_size,) + self.decoder_input_shape)
-		out = ConvLSTM2D(filters=64, kernel_size=3, return_sequences=True, activation='tanh', 
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', 
 			padding='same')([decoder_inputs, state_h, state_c])
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh')(out)
+		out = ConvLSTM2D(filters=25, kernel_size=3, return_sequences=True, activation='tanh')(out)
 
 		out = TimeDistributed(Flatten())(out)
 
 		num_output_features = 1
-		  # TODO: this gets a 2400x1 (12x2x2x50) vector, maybe it's worth reducing the dimensions in lstm layers?
 		out = TimeDistributed(Dense(num_output_features, activation='linear'))(out)
 
 		self.model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=out)

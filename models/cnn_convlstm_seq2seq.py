374908dd2647c147218cb18bb827b6a3f3727368
dzhv <dz.vysniauskas@gmail.com>
2019-06-07 10:46:55 +0100

    resuming win11_3 experiment

--- models/cnn_convlstm_seq2seq.py
+++ models/cnn_convlstm_seq2seq.py
@@ -28,11 +28,9 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 		out = TimeDistributed(Conv2D(50, kernel_size=3, activation='tanh', padding='same'))(out)
 
 		# encoder
-		out, h1, c1 = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, return_state=True, activation='tanh', 
-			padding='same')(out)
-		out, h2, c2 = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, return_state=True, activation='tanh', 
-			padding='same')(out)
-		encoder_outputs, h3, c3 = ConvLSTM2D(filters=50, kernel_size=3, activation='tanh', 
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
+		encoder_outputs, state_h, state_c = ConvLSTM2D(filters=50, kernel_size=3, activation='tanh', 
 			padding='same', return_state=True)(out)
 
 		# decoder
@@ -41,11 +39,9 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 		self.decoder_input_shape = (2, 2, 50)
 		decoder_inputs = Input(shape=(segment_size,) + self.decoder_input_shape)
 		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', 
-			padding='same')([decoder_inputs, h1, c1])
-		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', 
-			padding='same')([out, h2, c2])
-		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', 
-			padding='same')([out, h3, c3])
+			padding='same')([decoder_inputs, state_h, state_c])
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
+		out = ConvLSTM2D(filters=50, kernel_size=3, return_sequences=True, activation='tanh', padding='same')(out)
 
 		out = TimeDistributed(Flatten())(out)
 
@@ -75,7 +71,7 @@ class CnnConvLSTMSeq2Seq(KerasModel):
 	def form_targets(self, y):
 		return y[:, :, None]
 
-# model = CnnConvLSTMSeq2Seq(window_size=11)
-# output = model.forward(np.random.randn(1, 12, 11, 11))
-# print("output shape:")
-# print(output.shape)
+model = CnnConvLSTMSeq2Seq(window_size=11)
+output = model.forward(np.random.randn(1, 12, 11, 11))
+print("output shape:")
+print(output.shape)

3aca380a9d7e347232a36cc25b2506e549ce71aa
dzhv <dz.vysniauskas@gmail.com>
2019-06-24 12:03:10 +0100

    evaluating attention

--- run_gc_evaluator.sh
+++ run_gc_evaluator.sh
@@ -1,5 +1,5 @@
 python experiments/model_evaluator.py \
-        --model_file results/predrnn_pred12/saved_models/train_model_13 \
-        --model_name predrnn --batch_size 1 \
-		--output_size 30 --hiden_size 30 --num_layers 2 \
-		--shuffle_order false --prediction_batch_size 1  --evaluation_steps "10,12,30"
+        --model_file results/cnn_convlstm_attention/saved_models/train_model_latest \
+        --model_name cnn_convlstm_attention --batch_size 1000 \
+		--cnn_filters="20,40,40" --decoder_filters="10,64" --encoder_filters="40" \
+		--shuffle_order false --prediction_batch_size 10000  --evaluation_steps "10,12"

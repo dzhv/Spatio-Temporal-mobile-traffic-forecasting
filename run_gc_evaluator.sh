74d4cc6738c2772c101f3a7dddf3768794a02984
dzhv <dz.vysniauskas@gmail.com>
2019-06-29 11:54:55 +0100

    evaluating win11_3 for 30 steps

--- run_gc_evaluator.sh
+++ run_gc_evaluator.sh
@@ -1,5 +1,6 @@
 python experiments/model_evaluator.py \
-        --model_file results/cnn_convlstm_attention/saved_models/train_model_latest \
+        --model_file results/cnn_convlstm_seq2seq_win11_3/saved_models/train_model_40 \
         --model_name cnn_convlstm_attention --batch_size 1000 \
-		--cnn_filters="20,40,40" --decoder_filters="10,64" --encoder_filters="40" \
-		--shuffle_order false --prediction_batch_size 10000  --evaluation_steps "10,12"
+		--cnn_filters="25,50,50" --encoder_filters="50,50,50" --decoder_filters="50,50,50" \
+		--mlp_hidden_sizes="1" --output_size 30 \
+		--shuffle_order false --prediction_batch_size 10000  --evaluation_steps "10,12,30"

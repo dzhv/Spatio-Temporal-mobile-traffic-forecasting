da8995fc8d8f742e018f9c21cf5c5a05e5d987e6
dzhv <dz.vysniauskas@gmail.com>
2019-07-09 10:51:53 +0100

    removing state passing for attention

--- models/cnn_convlstm_attention.py
+++ models/cnn_convlstm_attention.py
@@ -15,11 +15,12 @@ from models import model_device_adapter
 from models.convlstm_attention_cell import ConvLSTMAttentionCell
 
 class CnnConvLSTMAttention(KerasModel):
-	def __init__(self, gpus=1, batch_size=50, segment_size=12, window_size=11,
+	def __init__(self, gpus=1, batch_size=50, segment_size=12, output_size=12, window_size=11,
 		cnn_filters=[25,50,50], encoder_filters=[50], decoder_filters=[50],
 		learning_rate=0.0001, learning_rate_decay=0, create_tensorboard=False):
 
 		self.segment_size = segment_size
+		self.output_size = output_size
 		self.gpus = gpus
 		
 		# Define an input sequence.
@@ -27,7 +28,8 @@ class CnnConvLSTMAttention(KerasModel):
 		encoder_inputs = Input(shape=(segment_size, window_size, window_size, 1))
 		
 		print(f"encoder_inputs: {encoder_inputs}")
-		out = TimeDistributed(Conv2D(cnn_filters[0], kernel_size=3, activation='tanh', padding='same'))(encoder_inputs)
+		out = TimeDistributed(Conv2D(cnn_filters[0], kernel_size=3, activation='tanh', 
+			padding='same'))(encoder_inputs)
 		out = TimeDistributed(AveragePooling2D())(out)
 		out = TimeDistributed(Conv2D(cnn_filters[1], kernel_size=3, activation='tanh', padding='same'))(out)
 		out = TimeDistributed(AveragePooling2D())(out)
@@ -43,12 +45,16 @@ class CnnConvLSTMAttention(KerasModel):
 		# encoder_outputs shape: (batch_size, segment_size, window_size, window_size, num_filters)
 
 		# decoder
+
+		latent_dim = window_size // 2**(len(cnn_filters) - 1)
+		self.decoder_input_shape = (latent_dim, latent_dim, encoder_filters[-1])
+		decoder_inputs = Input(shape=(output_size,) + self.decoder_input_shape, name="decoder_input")
 	
 		attention_layer = ConvRNN2D(ConvLSTMAttentionCell(decoder_filters[0], kernel_size=3, padding='same'), return_sequences=True)
 		attention_layer._num_constants = 1
 		# the second encoder_outputs is given as 'constants' to the layer,
 		# so that it can be fully transferred to the cell
-		out = attention_layer([encoder_outputs, state_h, state_c, encoder_outputs])
+		out = attention_layer([decoder_inputs, encoder_outputs])
 
 		for filters in decoder_filters[1:]:
 			out = ConvLSTM2D(filters=filters, kernel_size=3, return_sequences=True, activation='tanh', 
@@ -60,7 +66,7 @@ class CnnConvLSTMAttention(KerasModel):
 		out = TimeDistributed(Dense(100, activation='relu', kernel_regularizer=regularizers.l2(0.002)))(out)
 		out = TimeDistributed(Dense(num_output_features, activation='linear'))(out)
 
-		self.model = Model(inputs=encoder_inputs, outputs=out)
+		self.model = Model(inputs=[encoder_inputs, decoder_inputs], outputs=out)
 		self.model = model_device_adapter.get_device_specific_model(self.model, gpus)
 		
 		optimizer = Adam(lr=learning_rate, decay=learning_rate_decay)
@@ -73,16 +79,17 @@ class CnnConvLSTMAttention(KerasModel):
 	def form_model_inputs(self, x):
 		# adding an empty (channel) dimension to the end
 		encoder_input = np.expand_dims(x, axis=-1)
-		# (batch_size, segment_size, latent_dim, latent_dim, channels)
-		# decoder_input = np.zeros((encoder_input.shape[0], self.segment_size) + self.decoder_input_shape)
+
+		# (batch_size, output_size, latent_dim, latent_dim, channels)
+		decoder_input = np.zeros((encoder_input.shape[0], self.output_size) + self.decoder_input_shape)
 		
-		return encoder_input
+		return [encoder_input, decoder_input]
 
 	def form_targets(self, y):
 		return y[:, :, None]
 
 if __name__ == '__main__':
-	model = CnnConvLSTMAttention(window_size=11)
+	model = CnnConvLSTMAttention(window_size=11, output_size=3)
 	output = model.forward(np.random.randn(2, 12, 11, 11))
 	print("output shape:")
 	print(output.shape)

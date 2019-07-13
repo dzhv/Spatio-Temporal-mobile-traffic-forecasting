0b63524f995855f2f39a614576874f6ede19b304
dzhv <dz.vysniauskas@gmail.com>
2019-07-13 13:01:46 +0100

    some debugging stuff

--- experiments/arima_trainer.py
+++ experiments/arima_trainer.py
@@ -5,12 +5,14 @@ import statsmodels.api as sm
 import time
 
 def train_and_save():
+	print(f"starting")
 
-	os.system("mkdir results/arima")
-	os.system("mkdir results/arima/saved_models")
+	os.mkdir("results/arima")
+	os.mkdir("results/arima/saved_models")
 
 	p, d, q = 12, 1, 1
 
+	print(f"loading data")
 	train = np.load("data/train.npy")
 
 	window_size = train.shape[-1]
